211d0a24d823ec33afa2660d7fb1045bbd71e65f
Sasa - Ured <sasamikac@gmailcom>
2018-11-09 14:38:07 +0100

    Promjene - ured

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,7 @@
 
         <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                </a>
+                <img src="../images/SMDigital_Logo.png" height="40px">
 
                 <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
