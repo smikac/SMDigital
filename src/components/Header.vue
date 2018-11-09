9a4e2d5c1dcf458e4c84d5871c82e0ff5ba9547f
Sasa <sasamikac@gmail.com>
2018-11-09 12:42:05 +0100

    dodan meni

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,39 @@
 <template>
     <div>
         <h1>Header title</h1>
-        <nav>
-            <ul>
-                <li><router-link to="/">Home</router-link></li>
-                <li><router-link to="/Contact">Contact</router-link></li>       
-            </ul>
+
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+                </a>
+
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <router-link class="navbar-item" to="/">Home</router-link>
+                <router-link class="navbar-item" to="/Contact">Contact</router-link>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </div>
         </nav>
     </div>
 </template>
